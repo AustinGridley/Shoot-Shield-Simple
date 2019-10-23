b933ffe60762870db682a976b6cf6a6c6d7ef3a1
austingridley <austin.gridley@gmail.com>
2019-10-23 11:20:38 -0600

    'Original'

--- shootShieldReload.js
+++ shootShieldReload.js
@@ -134,71 +134,10 @@ function lucky()    {
         document.getElementById("luckyLocation").innerHTML = `You have ${userBulletCount} bullet(s). Your opponent has ${computerBulletCount} bullet(s)!`;
     }
 }
-
 function randomPick (num) {
     //this function rolls a dice based on their input and returns it
     let randomNumber = Math.random() * num /*"demo" or x or what?*/; 
     //let diceRoll = Math.random() * 6; 
     randomNumber = Math.ceil(randomNumber);
-    
     return randomNumber
     }
-
-
-
-    /*document.getElementById("dice1").innerHTML = dice1;
-    let dice2 = diceRandom;
-    document.getElementById("dice1").innerHTML = dice2;
-    let dice3 = diceRandom;
-    document.getElementById("dice1").innerHTML = dice3;
-    let dice4 = diceRandom;
-    document.getElementById("dice1").innerHTML = dice4;
-    let dice5 = diceRandom;
-    document.getElementById("dice1").innerHTML = dice5;
-    let dice6 = diceRandom;
-    document.getElementById("dice1").innerHTML = dice6;
-    console.log(dice1())
-    console.log(dice2())
-    console.log(dice3())
-    console.log(dice4())
-    console.log(dice5())
-    console.log(dice6())*/
-
-    /*function startOver()    {
-    document.getElementById("tryAgain").innerHTML = "Roll different sided dice?"
-}*/
-
-
-    /*function iconSelection(diceRoll) {
-    let y;
-    if ("demo"==1) {
-        y = <i class="fas fa-dice-one"></i>
-        
-    }
-    if (diceRoll==2) {
-        y = <i class="fas fa-dice-two"></i>
-        
-    }
-    if (diceRoll==3) {
-        y = <i class="fas fa-dice-three"></i>
-        
-    }
-    if (diceRoll==4) {
-        y = <i class="fas fa-dice-four"></i>
-        
-    }
-    if (diceRoll==5) {
-        y = <i class="fas fa-dice-five"></i>
-        
-    }
-    if (diceRoll==6) {
-        y = `<i class="fas fa-dice-six"></i>`
-        
-    }
-    if (diceRoll>6) {
-        y = `<i class="fas fa-dice-dice"></i>`
-        
-    }
-    document.getElementById("diceIcon").innerHTML = y;
-}
-*/
